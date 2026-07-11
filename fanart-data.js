19dfd055a2cf4ce45d4157bd2fe7dda6d38dee53
BTmochi <52621330+BTmochi@users.noreply.github.com>
2026-07-11 18:14:33 +0900

    Move fanart data to external file

---
+++ fanart-data.js
@@ -0,0 +1,16 @@
+// ---- Fanart data ----
+// Only artwork confirmed safe-for-work is included here.
+window.fanartData = [
+  { img: "img/fanart-01.jpg", author: "古都こと", handle: "@waa_waa_wa", date: "", caption: "まいくろびきに…", url: "https://x.com/waa_waa_wa/status/2038611814959997062" },
+  { img: "img/fanart-02.jpg", author: "古都こと", handle: "@waa_waa_wa", date: "", caption: "この服好きっていう絵", url: "https://x.com/waa_waa_wa/status/2039351999247097992" },
+  { img: "img/fanart-03.jpg", author: "古都こと", handle: "@waa_waa_wa", date: "", caption: "ぼたもちーのさん！", url: "https://twitter.com/waa_waa_wa/status/2037991706080243868" },
+  { img: "img/fanart-04.jpg", author: "古都こと", handle: "@waa_waa_wa", date: "", caption: "", url: "https://twitter.com/waa_waa_wa/status/2038992333040148721" },
+  { img: "img/fanart-05.jpg", author: "古都こと", handle: "@waa_waa_wa", date: "", caption: "ぱーかー", url: "https://x.com/waa_waa_wa/status/2040731745520329167" },
+  { img: "img/fanart-06.jpg", author: "古都こと", handle: "@waa_waa_wa", date: "", caption: "にこにこかわいい", url: "https://twitter.com/waa_waa_wa/status/2040778678997811213" },
+  { img: "img/fanart-07.jpg", author: "古都こと", handle: "@waa_waa_wa", date: "", caption: "もちーのせーらー", url: "https://x.com/waa_waa_wa/status/2043287362814525817" },
+  { img: "img/fanart-08.jpg", author: "ことさぶ郎", handle: "@furuichi_koto25", date: "", caption: "抱き枕(非売品)", url: "https://x.com/furuichi_koto25/status/2043697074458550380" },
+  { img: "img/fanart-09.jpg", author: "古都こと", handle: "@waa_waa_wa", date: "", caption: "もちもち～", url: "https://x.com/waa_waa_wa/status/2045863876210909664" },
+  { img: "img/fanart-11.jpg", author: "古都こと", handle: "@waa_waa_wa", date: "", caption: "ほろよいーの", url: "https://x.com/waa_waa_wa/status/2056016849993879848" },
+  { img: "img/fanart-10.jpg", author: "ちの", handle: "@chino2047", date: "", caption: "可愛すぎて描いた", url: "https://twitter.com/chino2047/status/2061335771102203921" },
+  { img: "img/fanart-12.jpg", author: "とととい", handle: "@8tototoi8", date: "", caption: "ぼたもちーのさん！", url: "https://x.com/8tototoi8/status/2075868890803077121" }
+];
